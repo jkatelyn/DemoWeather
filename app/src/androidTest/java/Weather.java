8370dd027c2aa762f915bd08bc46b309727bc063
JENNIFER KATELYN JOSEPH JEBASEELAN <21033841@myrp.edu.sg>
2022-08-16 19:00:50 +0800

    Initial commit

---
+++ app/src/androidTest/java/Weather.java
@@ -0,0 +1,28 @@
+public class Weather {
+    private String area;
+    private String forecast;
+
+    public Weather(String area, String forecast) {
+        this.area = area;
+        this.forecast = forecast;
+    }
+
+    public String getArea() {
+        return area;
+    }
+
+    public String getForecast() {
+        return forecast;
+    }
+
+    public void setArea(String area) {
+        this.area = area;
+    }
+    public void setForecast(String forecast) {
+        this.forecast = forecast;
+    }
+
+    @Override
+    public String toString() { return "Area:" + area + ", " + forecast;  }
+
+}
